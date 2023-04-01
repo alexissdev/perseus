f8e8c6a500ae9b5ea5ff2f078b5d156ee550ebcb
alexissdev <notcacha1998@gmail.com>
2023-04-01 18:51:04 -0300

    feat(model): added the Product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,19 @@
+import Model from "./model";
+import { Schema, model } from "mongoose";
+
+export default interface Product extends Model {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+export const ProductSchema = model<Product>(
+  "Product",
+  new Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  })
+);
