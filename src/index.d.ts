c7e7fa26222971cdd197c9b9709eef8e7cd93185
alexissdev <notcacha1998@gmail.com>
2023-04-01 18:52:13 -0300

    feat(types): added the IProduct types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
-import Model from './models/model';
+import Model from "./models/model";
 import User from "./models/user.model";
+import Product from "./models/product.model";
 
 export type IModel = Model;
 export type IUser = User;
+export type IProduct = Product;
