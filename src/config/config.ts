abac39c7aa89957ba5ed4576548eeb1d99cd8a32
alexissdev <notcacha1998@gmail.com>
2023-04-04 11:15:07 -0300

    feat(config): added the api prefix

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,7 @@ export default {
   port: (process.env.PORT as unknown) as number || 3000,
   databaseURL: process.env.MONGODB_URI || "mongodb://localhost:27017/test",
   jwtSecret: process.env.JWT_SECRET || "secret",
+  api: {
+    prefix: "/api"
+  }
 }
--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
+import config from "../config/config";
 import api from "../api/index";
 
 export default () => {
   const app: Application = express();
 
   app.use(express.json());
-  app.use("/api", api());
+  app.use(config.api.prefix, api());
 
   return app;
 }
