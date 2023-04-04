57f119023ebb2a8c42e3c8a47799f331241cfb2a
alexissdev <notcacha1998@gmail.com>
2023-04-04 11:26:19 -0300

    fix(config): fix the env variable of mongo url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
   port: (process.env.PORT as unknown) as number || 3000,
-  databaseURL: process.env.MONGODB_URI || "mongodb://localhost:27017/test",
+  databaseURL: process.env.MONGO_URL || "mongodb://localhost:27017/test",
   jwtSecret: process.env.JWT_SECRET || "secret",
   api: {
     prefix: "/api"
